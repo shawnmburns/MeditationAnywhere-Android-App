0832230723428e4accaa6f0efdd05a003f16cd3d
Shawn Burns <shawn.m.burns@gmail.com>
2012-12-27 13:02:30 -0500

    First Commit of MA Work Stress App

---
+++ src/com/meditatenow/views/MNBaseActivity.java
@@ -0,0 +1,7 @@
+package com.meditatenow.views;
+
+import com.vl.ctrls.VLBaseActivity;
+
+public class MNBaseActivity extends VLBaseActivity {
+
+}
---
+++ src/com/meditatenow/views/MNTabView.java
@@ -0,0 +1,14 @@
+package com.meditatenow.views;
+
+import com.meditatenow.R;
+
+import android.content.Context;
+
+public class MNTabView extends MNBaseView {
+
+	public MNTabView(Context context) {
+		super(context);
+		
+		this.setBackgroundResource(R.drawable.bg);
+	}
+}
